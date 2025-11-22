fce6ab02572d4f5fcffa39e449ab58d02b993231
KavindyaTharushi <kavindyahettiarachchi2001@gmail.com>
2025-11-22 20:31:03 +0530

    Add new files

---
+++ main.py
@@ -0,0 +1,117 @@
+import streamlit as st
+from phi.agent import Agent
+from phi.model.google import Gemini
+from phi.tools.duckduckgo import DuckDuckGo
+from google.generativeai import upload_file, get_file
+import google.generativeai as genai
+import time
+import os
+from pathlib import Path
+import tempfile
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+if API_KEY:
+    # Fix for PHI Gemini (important!)
+    os.environ["GOOGLE_API_KEY"] = API_KEY
+
+    # Fix for google.generativeai
+    genai.configure(api_key=API_KEY)
+else:
+    st.error("GOOGLE_API_KEY not found in .env file")
+
+
+st.set_page_config(
+    page_title="Video Summarizer Agent",
+    layout= "wide"
+
+)
+
+st.title("Video Summarizer Agent")
+st.header("This app is powered b Gemini")
+
+def initialize_agent():
+    return Agent(
+        name = "Video Summarizer Agent",
+        model = Gemini(id="gemini-2.5-flash"),
+        tools = [DuckDuckGo()],
+        markdown = True
+
+    )
+
+multimodel_Agent = initialize_agent()
+
+video_file = st.file_uploader(
+    "Upload a Video file here",type=['mp4','mov','avi'], help= "Upload a Video for AI Analysis"
+
+)
+
+if video_file:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
+        temp_video.write(video_file.read())
+        video_path = temp_video.name
+    
+    st.video(video_path,format="video/mp4",start_time=0)
+
+    user_query = st.text_area(
+        "What insights are you seeking from the video?",
+        placeholder="Ask anything about the video content. The AI agent will analyze and gather additional conten",
+        help= "Provide specific questions or insights you want from the video"
+        
+    )
+
+    if st.button("Analyze video",key="analyze_video_button"):
+        if not user_query:
+            st.warning("Please enter a question or insight to analyze the video")
+        else:
+            try:
+                with st.spinner("Processing video and gathering insights..."):
+                    processed_video = upload_file(video_path)
+                    while processed_video.state.name == "PROCESSING":
+                        time.sleep(1)
+                        processed_video = get_file(processed_video.name)
+
+                    #prompt generation for analysis
+                    analysis_prompt = (
+                        f"""
+                        Analyze the uploaded video for content and context.
+                        Respond to the following query using video insight and supplementary web research:
+                        {user_query}
+
+                        Provide a detailed, user-friendly, and actionable response.user_query
+                        """
+                    )
+
+                    # AI agent Processing 
+                    response = multimodel_Agent.run(analysis_prompt, videos=[processed_video])
+                
+                #Display the result
+                st.subheader("Analysis Result")
+                st.markdown(response.content)
+
+            except Exception as error:
+                st.error(f"An error occurred during analysis:{error}")
+            finally:
+                #cleanup up temporary video
+                Path(video_path).unlink(missing_ok=True)
+        
+    else:
+        st.info("Upload a video file to begin analysis")
+
+
+# Customizer text area height
+st.markdown(
+    """ 
+     <style>
+     .stTextArea textarea {
+          height:100px;
+
+     }
+     </style>
+
+    """,
+    unsafe_allow_html=True
+)
